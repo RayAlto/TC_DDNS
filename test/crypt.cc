dd7b203833bab56b9bf95cf8e44d5369d859f5bd
RayAlto <tianjiayu668@gmail.com>
2023-03-10 13:41:14 +0800

    update test for tc::crypt

--- test/crypt.cc
+++ test/crypt.cc
@@ -15,5 +15,12 @@ int main(int argc, const char *argv[]) {
                 std::vector<std::uint8_t> {0x19, 0x19, 0x81, 0x00}))
             == "cba51e0b3e4550e9d6a25ad6f610fcbbe81d3217b388886fdd98542f54271ede")
         << std::endl;
+
+    // echo -ne '114514' | openssl dgst -sha256
+    std::cout
+        << std::boolalpha
+        << (tc::util::data2hexstr(tc::crypt::sha256("114514"))
+            == "384fde3636e6e01e0194d2976d8f26410af3e846e573379cb1a09e2f0752d8cc")
+        << std::endl;
     return 0;
 }
